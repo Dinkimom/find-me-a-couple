4303e6eca7f5f8ccf2d19ebdb629720c4deccd55
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2021-01-15 02:14:14 +0300

    Fix get chats

--- src/routes/ChatsRouter.ts
+++ src/routes/ChatsRouter.ts
@@ -12,26 +12,31 @@ const entity = EntityEnum.Chats;
 chatsRouter.get('/', async (req: Request, res: Response) => {
   const collection = getCollection(entity);
 
+  const user_id = String((req as any).user._id);
+
   const chats = await collection
-    .find({ participants: { $all: [(req as any).user._id] } })
+    .find({ participants: { $all: [user_id] } })
     .toArray();
 
   const completeChats: ChatsDto = [];
 
-  chats.forEach(async (chat, index) => {
-    const companionId = chat.participants.filter(
-      (userId: string) => userId !== (req as any).user._id
-    )[0];
+  const usersCollection = getCollection(EntityEnum.Users);
 
-    const usersCollection = getCollection(EntityEnum.Users);
+  const users = await usersCollection.find({}).toArray();
 
-    const companion = await usersCollection.findOne({
-      _id: new ObjectId(companionId),
-    });
+  chats.forEach((chat) => {
+    const companionId = chat.participants.find(
+      (userId: string) => userId !== user_id
+    );
 
-    if (companion) {
+    const companion = users.find(
+      (item) => String(item._id) === String(companionId)
+    );
+
+    if (companion && chat.messages.length) {
       completeChats.push({
         companion: {
+          _id: companionId,
           name: companion.name,
           image: companion.image,
         },
@@ -39,52 +44,56 @@ chatsRouter.get('/', async (req: Request, res: Response) => {
         messages: chat.messages,
       });
     }
-
-    if (index === chats.length - 1) {
-      res.status(200).send({ result: completeChats });
-    }
   });
+
+  res.status(200).send({ result: completeChats });
 });
 
 chatsRouter.get('/:receiver', async (req: Request, res: Response) => {
   const { receiver } = req.params;
 
-  if (receiver === (req as any).user._id) {
+  const user_id = String((req as any).user._id);
+
+  if (receiver === user_id) {
     return res.status(404).send();
   }
 
   const collection = getCollection(entity);
 
   let chat = await collection.findOne({
-    participants: { $all: [receiver, (req as any).user._id] },
+    participants: { $all: [receiver, user_id] },
   });
 
   if (!chat) {
     await collection.insertOne({
-      participants: [receiver, (req as any).user._id],
+      participants: [receiver, user_id],
       messages: [],
     });
 
     chat = await collection.findOne({
-      participants: { $all: [receiver, (req as any).user._id] },
+      participants: { $all: [receiver, user_id] },
     });
   }
 
   const usersCollection = getCollection(EntityEnum.Users);
 
-  const userId = new ObjectId(receiver);
+  try {
+    const userId = new ObjectId(receiver);
 
-  const user = await usersCollection.findOne({ _id: userId });
+    const user = await usersCollection.findOne({ _id: userId });
 
-  if (user) {
-    res.status(200).send({
-      result: {
-        companion: { name: user.name, image: user.image },
-        lastMessage: chat.messages[chat.messages.length - 1],
-        messages: chat.messages,
-      },
-    });
-  } else {
+    if (user) {
+      res.status(200).send({
+        result: {
+          companion: { name: user.name, image: user.image },
+          lastMessage: chat.messages[chat.messages.length - 1],
+          messages: chat.messages,
+        },
+      });
+    } else {
+      res.status(404).send();
+    }
+  } catch {
     res.status(404).send();
   }
 });
@@ -92,21 +101,23 @@ chatsRouter.get('/:receiver', async (req: Request, res: Response) => {
 chatsRouter.post('/:receiver', async (req: Request, res: Response) => {
   const { receiver } = req.params;
 
+  const user_id = String((req as any).user._id);
+
   const collection = getCollection(entity);
 
   const chat = (
     await collection
-      .find({ participants: { $all: [receiver, (req as any).user._id] } })
+      .find({ participants: { $all: [receiver, user_id] } })
       .toArray()
   )[0];
 
   if (chat) {
     const { messages } = chat;
 
-    messages.push({ user_id: (req as any).user._id, ...req.body });
+    messages.push({ user_id, ...req.body });
 
     collection.updateOne(
-      { participants: { $all: [receiver, (req as any).user._id] } },
+      { participants: { $all: [receiver, user_id] } },
       { $set: { messages } }
     );
 
